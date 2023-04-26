7666cf971aca56c508cebf7180616ca0c41423e3
Awat Singdamrong <110981996+pinkyPingy@users.noreply.github.com>
2023-04-26 17:33:29 +0700

    feat: connect frontend to db

--- script_editable_todolist.js
+++ script_editable_todolist.js
@@ -167,11 +167,124 @@ const getItemsFromDB = async () => {
     .catch((error) => console.error(error));
 };
 
+const showItemsInTable = (itemsData) => {
+  const no_status = document.getElementById("no-status");
+  no_status.innerHTML = "";
+  const next_up = document.getElementById("next-up");
+  next_up.innerHTML = "";
+  const in_progress = document.getElementById("in-progress");
+  in_progress.innerHTML = "";
+  const com_pleted = document.getElementById("com-pleted");
+  com_pleted.innerHTML = "";
+  // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //
+  const items = itemsData;
+  items.sort((a, b) => (a.priority < b.priority) ? 1 : -1);
+  // ----------------- FILL IN YOUR CODE ABOVE THIS AREA ONLY ----------------- //
+  itemsData.map((item) => {
+    // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //
+    let priority;
+    let priority_2;
+    switch (item.priority) {
+      case 1:
+        priority = "low";
+        priority_2 = "Low";
+        break;
+      case 2:
+        priority = "medium";
+        priority_2 = "Medium";
+        break;
+      case 3:
+        priority = "high";
+        priority_2 = "High";
+        break;
+      default:
+        priority = "completed";
+        priority_2 = "Completed";
+    }
+    if (item.status == "No Status" || item.status == "") {
+      no_status.innerHTML += `
+        <div class="box" onclick="PopUpOnClick()">
+          <h1 class="box-title hover-1">${item.title}</h1>
+          <h2 class="box-title-2">${item.course_id}</h2>
+          <div class="box-priority-${priority}">
+            <h1>${priority_2}</h1>
+          </div>
+          <h1 class="box-date">📆 ${item.due_date}</h1>
+        </div>
+          `;
+    }
+    else if (item.status == "Next Up") {
+      next_up.innerHTML += `
+        <div class="box" onclick="PopUpOnClick()">
+          <h1 class="box-title hover-1">${item.title}</h1>
+          <h2 class="box-title-2">${item.course_id}</h2>
+          <div class="box-priority-${priority}">
+            <h1>${priority_2}</h1>
+          </div>
+          <h1 class="box-date">📆 ${item.due_date}</h1>
+        </div>
+          `;
+    }
+    else if (item.status == "In Progress") {
+      next_up.innerHTML += `
+        <div class="box" onclick="PopUpOnClick()">
+          <h1 class="box-title hover-1">${item.title}</h1>
+          <h2 class="box-title-2">${item.course_id}</h2>
+          <div class="box-priority-${priority}">
+            <h1>${priority_2}</h1>
+          </div>
+          <h1 class="box-date">📆 ${item.due_date}</h1>
+        </div>
+          `;
+    }
+    else if (item.status == "Completed") {
+      priority = "completed";
+      priority_2 = "Completed";
+      com_pleted.innerHTML += `
+        <div class="box" onclick="PopUpOnClick()">
+          <h1 class="box-title hover-1">${item.title}</h1>
+          <h2 class="box-title-2">${item.course_id}</h2>
+          <div class="box-priority-${priority}">
+            <h1>${priority_2}</h1>
+          </div>
+          <h1 class="box-date">📆 ${item.due_date}</h1>
+        </div>
+          `;
+    }
+    // ----------------- FILL IN YOUR CODE ABOVE THIS AREA ONLY ----------------- //
+  });
+  no_status.innerHTML += `
+    <div class="box" onclick="PopUpOnClick()">
+      <h1 class="box-add-new-list">Add new list</h1>
+    <div class="box-add-new-list-2">⊕</div>
+    </div>
+      `;
+  next_up.innerHTML += `
+    <div class="box" onclick="PopUpOnClick()">
+      <h1 class="box-add-new-list">Add new list</h1>
+    <div class="box-add-new-list-2">⊕</div>
+    </div>
+      `;
+  in_progress.innerHTML += `
+    <div class="box" onclick="PopUpOnClick()">
+      <h1 class="box-add-new-list">Add new list</h1>
+    <div class="box-add-new-list-2">⊕</div>
+    </div>
+      `;
+  com_pleted.innerHTML += `
+    <div class="box" onclick="PopUpOnClick()">
+      <h1 class="box-add-new-list">Add new list</h1>
+    <div class="box-add-new-list-2">⊕</div>
+    </div>
+      `;
+};
+
 document.addEventListener("DOMContentLoaded", async function (event) {
   // console.log("Showing group members.");
   // await showGroupMembers();
   console.log("Showing items from database.");
   await getItemsFromDB();
-  // showItemsInTable(itemsData);
+  console.log(itemsData);
+  showItemsInTable(itemsData);
 });
 //ping above------------------------------------------------------------
