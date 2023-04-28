104a86cbf6fb0c05de74a61ef24705a53ac926af
Pittinan Harnsinghkun <pittinan.har@gmail.com>
2023-04-28 13:03:32 +0700

    fix: merge conflict

--- script_editable_todolist.js
+++ script_editable_todolist.js
@@ -2,15 +2,9 @@
 const backendIPAddress = "127.0.0.1:3000";
 const frontendIPAddress = "127.0.0.1:5500";
 
-function PopUpOnClick() {
-  myPopup.classList.add("show");
-}
-function PopDownOnClick() {
-  myPopup.classList.remove("show");
-}
-
-//ping below------------------------------------------------------------
 let itemsData;
+const CourseTitle = new Set();
+
 let student_id;
 const getUserId = async () => {
   const options = {
@@ -44,13 +38,16 @@ const getItemsFromDB = async () => {
   await fetch(`http://${backendIPAddress}/items/`, options)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       itemsData = data.filter(item => item.student_id === student_id);
+      // console.log(itemsData)
     })
     .catch((error) => console.error(error));
 };
 
 const showItemsInTable = (itemsData) => {
+  PopUpOnClick("No Status");
+  PopDownOnClick();
+
   const no_status = document.getElementById("no-status");
   no_status.innerHTML = "";
   const next_up = document.getElementById("next-up");
@@ -59,16 +56,17 @@ const showItemsInTable = (itemsData) => {
   in_progress.innerHTML = "";
   const com_pleted = document.getElementById("com-pleted");
   com_pleted.innerHTML = "";
+  const subjectLists = document.getElementById("title-to-add")
+  subjectLists.innerHTML = "<option value='0'>Subject</option>";
+  const priorityLists = document.getElementById("priority-to-add")
+  priorityLists.innerHTML = "<option value='0'>Low</option>";
 
   if (student_id === "") {
     console.log("No student_id ahhhh");
   }
-  // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //
   const items = itemsData;
   items.sort((a, b) => (a.priority < b.priority) ? 1 : -1);
-  // ----------------- FILL IN YOUR CODE ABOVE THIS AREA ONLY ----------------- //
   items.map((item) => {
-    // ----------------- FILL IN YOUR CODE UNDER THIS AREA ONLY ----------------- //
     let prior;
     let prior_2;
     switch (item.priority) {
@@ -84,95 +82,117 @@ const showItemsInTable = (itemsData) => {
         prior = "high";
         prior_2 = "High";
         break;
-      default:
+      case 3:
         prior = "completed";
         prior_2 = "Completed";
     }
     if (item.status == "No Status" || item.status == "") {
-      no_status.innerHTML += `
-        <div class="box" onclick="PopUpOnClick()">
-          <h1 class="box-title hover-1">${item.title}</h1>
-          <h2 class="box-title-2">${item.course_id}</h2>
-          <div class="box-priority-${prior}">
-            <h1>${prior_2}</h1>
-          </div>
-          <h1 class="box-date">📆 ${item.due_date}</h1>
-        </div>
-          `;
+      createNewTask(no_status, item, prior, prior_2)
     }
     else if (item.status == "Next Up") {
-      next_up.innerHTML += `
-        <div class="box" onclick="PopUpOnClick()">
-          <h1 class="box-title hover-1">${item.title}</h1>
-          <h2 class="box-title-2">${item.course_id}</h2>
-          <div class="box-priority-${prior}">
-            <h1>${prior_2}</h1>
-          </div>
-          <h1 class="box-date">📆 ${item.due_date}</h1>
-        </div>
-          `;
+      createNewTask(next_up, item, prior, prior_2)
     }
     else if (item.status == "In Progress") {
-      next_up.innerHTML += `
-        <div class="box" onclick="PopUpOnClick()">
-          <h1 class="box-title hover-1">${item.title}</h1>
-          <h2 class="box-title-2">${item.course_id}</h2>
-          <div class="box-priority-${prior}">
-            <h1>${prior_2}</h1>
-          </div>
-          <h1 class="box-date">📆 ${item.due_date}</h1>
-        </div>
-          `;
+      createNewTask(in_progress, item, prior, prior_2)
     }
     else if (item.status == "Completed") {
-      prior = "completed";
-      prior_2 = "Completed";
-      com_pleted.innerHTML += `
-        <div class="box" onclick="PopUpOnClick()">
-          <h1 class="box-title hover-1">${item.title}</h1>
-          <h2 class="box-title-2">${item.course_id}</h2>
-          <div class="box-priority-${prior}">
-            <h1>${prior_2}</h1>
-          </div>
-          <h1 class="box-date">📆 ${item.due_date}</h1>
-        </div>
-          `;
+      createNewTask(com_pleted, item, prior, prior_2)
     }
-    // ----------------- FILL IN YOUR CODE ABOVE THIS AREA ONLY ----------------- //
+
+    if (!(CourseTitle.has(item.title))) {
+      CourseTitle.add(item.title);
+      subjectLists.innerHTML += `<option value='${item.title}'>${item.title}</option>`;
+    }
+
   });
-  no_status.innerHTML += `
-    <div class="box" onclick="PopUpOnClick()">
-      <h1 class="box-add-new-list">Add new list</h1>
-    <div class="box-add-new-list-2">⊕</div>
-    </div>
-      `;
-  next_up.innerHTML += `
-    <div class="box" onclick="PopUpOnClick()">
-      <h1 class="box-add-new-list">Add new list</h1>
-    <div class="box-add-new-list-2">⊕</div>
-    </div>
-      `;
-  in_progress.innerHTML += `
-    <div class="box" onclick="PopUpOnClick()">
-      <h1 class="box-add-new-list">Add new list</h1>
-    <div class="box-add-new-list-2">⊕</div>
-    </div>
-      `;
-  com_pleted.innerHTML += `
-    <div class="box" onclick="PopUpOnClick()">
-      <h1 class="box-add-new-list">Add new list</h1>
-    <div class="box-add-new-list-2">⊕</div>
-    </div>
-      `;
+
+  createAddTask(no_status, 0);
+  createAddTask(next_up, 1);
+  createAddTask(in_progress, 2);
+  createAddTask(com_pleted, 3);
+
+  priorityLists.innerHTML += `<option value='1'>Medium</option>`;
+  priorityLists.innerHTML += `<option value='2'>High</option>`;
+  priorityLists.innerHTML += `<option value='3'>Completed</option>`;
 };
 
-const addItem = async () => {
-  const course_id = document.getElementById("course-id-to-add").value;
-  const status = document.getElementById("status-to-add").value;
-  const priority = document.getElementById("priority-to-add").value;
+const matchCourseTitletoId = (CourseTitle) => {
+  const items = itemsData;
+  items.map((item) => {
+    if (item.title == CourseTitle) return item.course_id;
+  });
+}
+
+const createNewTask = (status, item, prior, prior_2) => {
+  status.innerHTML += `
+  <div class="box" onclick="PopUpOnClick()">
+    <h1 class="box-title hover-1">${item.title}</h1>
+    <h2 class="box-title-2">${item.description}</h2>
+    <div class="box-priority-${prior}">
+      <h1>${prior_2}</h1>
+    </div>
+    <h1 class="box-date">📆 ${item.due_date}</h1>
+    <button id="deleteTask" onclick="deleteItem('${item.item_id}')">
+    Delete task
+  </button>
+  </div>
+    `;
+  }
+  
+const createAddTask = (status, status_i) => {
+  switch (status_i) {
+    case 0:
+      prior = "No Status";
+      break;
+    case 1:
+      prior = "Next Up";
+      break;
+    case 2:
+      prior = "In Progress";
+      break;
+    default:
+      prior = "Completed";
+  }
+  status.innerHTML += `
+  <div class="box" onclick="PopUpOnClick('${prior}')">
+    <h1 class="box-add-new-list">Add new list</h1>
+  <div class="box-add-new-list-2">⊕</div>
+  </div>
+    `;
+}
+
+function PopUpOnClick(taskStatus) {
+  insertInputBox.innerHTML = `
+  <input
+    type="text"
+    id="new-title-to-add"
+    name="new-title-to-add"
+    placeholder="or add new subject"
+  />
+  `;
+  insertButton.innerHTML = `
+  <button id="addTask" onclick="addItem('${taskStatus}')">
+    Add task
+  </button>
+  `;
+  addNewTaskPopUp.classList.add("show");
+}
+
+function PopDownOnClick() {
+  addNewTaskPopUp.classList.remove("show");
+}
+
+const addItem = async (taskStatus) => {
+  const priority = parseInt(document.getElementById("priority-to-add").value);
   const description = document.getElementById("description-to-add").value;
   const due_date = document.getElementById("due-date-to-add").value;
-  const title = document.getElementById("title-to-add").value;
+  let title = document.getElementById("title-to-add").value;
+  if (document.getElementById("new-title-to-add").value != '') {
+    title = document.getElementById("new-title-to-add").value;
+    document.getElementById("new-title-to-add").value = '';
+  }
+  const course_id = matchCourseTitletoId(title);
+  const status = taskStatus;
 
   const itemToAdd = {
     course_id: course_id,
@@ -195,8 +215,8 @@ const addItem = async () => {
 
   await fetch(`http://${backendIPAddress}/items`, options)
     .then((response) => {
-      document.getElementById("course-id-to-add").value = 0;
-      document.getElementById("status-to-add").value = 0;
+      // document.getElementById("course-id-to-add").value = 0;
+      // document.getElementById("status-to-add").value = 0;
       document.getElementById("priority-to-add").value = 0;
       document.getElementById("description-to-add").value = "";
       document.getElementById("due-date-to-add").value = "";
@@ -224,6 +244,7 @@ const deleteItem = async (item_id) => {
   console.log("Showing items from database.");
   await getItemsFromDB();
   showItemsInTable(itemsData);
+  console.log(itemsData);
 };
 
 const initTodo = async () => {
@@ -234,4 +255,3 @@ const initTodo = async () => {
   console.log(itemsData);
   showItemsInTable(itemsData);
 };
-//ping above------------------------------------------------------------
